56b0008482c7d73544888a6ad2f4acec8a78bbac
WillaG-E <willa.galipeaueldridge@maine.edu>
2025-03-07 07:51:37 -0500

    Implements a parameterized constructor

--- Book.java
+++ Book.java
@@ -2,5 +2,8 @@ public class Book{
     public String title;
     public String genre;
 
-    
+    public Book(String title, String genre){
+        this.title = title;
+        this.genre = genre;
+    }
 }
