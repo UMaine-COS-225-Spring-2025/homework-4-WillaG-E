b453315bc1075d05508e87bdc1a600c4b22c8f08
WillaG-E <willa.galipeaueldridge@maine.edu>
2025-03-07 08:06:23 -0500

    Print both BookShelf objects

--- LibraryTester.java
+++ LibraryTester.java
@@ -2,5 +2,8 @@ public class LibraryTester{
     public static void main(String[] args){
         BookShelf shelfO = new BookShelf('O');
         BookShelf shelfT = new BookShelf('T');
+    
+        System.out.println(shelfO);
+        System.out.println(shelfT);
     }
 }
