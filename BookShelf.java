05b7a1870ec87347478218e5605da78e44c8f801
WillaG-E <willa.galipeaueldridge@maine.edu>
2025-03-07 07:56:21 -0500

    Implements a default constructor

--- BookShelf.java
+++ BookShelf.java
@@ -3,4 +3,9 @@ import java.util.ArrayList;
 public class BookShelf {
     public char firstLetter;
     public ArrayList<Book> books;
+
+    public BookShelf(char firstLetter){
+        this.firstLetter = firstLetter;
+        this.books = new ArrayList<>(8);
+    }
 }
